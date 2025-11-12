dec2a604c3dfcc4d2c66a723d63671b44c0d7d4d
FulkorRain <ranygamer353@gmail.com>
2025-11-12 15:25:19 +0300

    Implemented File convertion to .bin files. With Folders support.

--- ConvertionEngine.cs
+++ ConvertionEngine.cs
@@ -8,10 +8,32 @@ namespace Discord_Splitter
 {
     internal class ConvertionEngine
     {
-       public void StartEngine(bool splitter = true, double splitterValue = 9.5)
+        private readonly string inputFolder = "Input";
+        private readonly string outputFolder = "Output";
+        public void StartEngine(bool splitter = true, double splitterValueMB = 9.5)
         {
-            Console.WriteLine("Hi!");
-            Console.WriteLine("Hi!");
+            long splitterValue = (long)(splitterValueMB * 1024 * 1024);
+
+            foreach (var file in Directory.EnumerateFiles(inputFolder, "*", SearchOption.AllDirectories))
+            {
+                string relativePath = Path.GetRelativePath(inputFolder, file);
+                string fileName = Path.GetFileName(file);
+                string fileExtension = Path.GetExtension(file);
+
+                string outputPath = Path.Combine(outputFolder, $"{relativePath}.bin");
+                long fileSize = new FileInfo(file).Length;
+
+                if (splitter == false || fileSize <= splitterValue)
+                {
+                    byte[] bytes = File.ReadAllBytes(file);
+                    File.WriteAllBytes(outputPath, bytes);
+                }
+                else
+                {
+                    // TODO: Splitter.
+                }
+
+            }
         }
     }
 }
